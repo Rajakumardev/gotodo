313fd3ed369756e3ba9556aaf562e7bf913a97b4
Rajakumardev <rajakumarofficial@gmail.com>
2023-10-02 11:55:59 +0530

    ping

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	const PORT string = ":8080"
 	fmt.Println("Booting up gotodo api server...")
+	app := fiber.New()
+	app.Get("/ping", func(c *fiber.Ctx) error {
 
+		response := map[string]string{
+			"method": "ping",
+			"status": "200",
+		}
+		return c.JSON(response)
+	})
+	app.Listen(PORT)
 }
