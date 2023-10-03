fa974b4dbfb18941d44e73d3207936545d724883
Rajakumardev <rajakumarofficial@gmail.com>
2023-10-03 09:14:34 +0530

    validation check added

--- models/todo.model.go
+++ models/todo.model.go
@@ -1,7 +1,32 @@
 package models
 
+import "github.com/go-playground/validator/v10"
+
 type Todo struct {
 	ID          int64  `json:"id,omitempty" gorm:"primaryKey`
 	Description string `json:"description,omitempty"`
 	Complted    bool   `json:"complted,omitempty" gorm:"default:false"`
+}
+
+var validate = validator.New()
+
+type ErrorResponse struct {
+	Field string `json:"field"`
+	Tag   string `json:"tag"`
+	Value string `json:"value,omitempty"`
+}
+
+func ValidateStruct[T any](payload T) []*ErrorResponse {
+	var errors []*ErrorResponse
+	err := validate.Struct(payload)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var element ErrorResponse
+			element.Field = err.StructNamespace()
+			element.Tag = err.Tag()
+			element.Value = err.Param()
+			errors = append(errors, &element)
+		}
+	}
+	return errors
 }
